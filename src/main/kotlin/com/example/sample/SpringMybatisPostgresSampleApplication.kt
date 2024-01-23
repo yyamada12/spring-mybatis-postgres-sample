8ab43a9dccad203d23c472540a7cef1889acbc12
yyamada12 <12yacropolisy@gmail.com>
2024-01-23 16:49:20 +0900

    initial commit, create with spring initializr

---
+++ src/main/kotlin/com/example/sample/SpringMybatisPostgresSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMybatisPostgresSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMybatisPostgresSampleApplication>(*args)
+}
